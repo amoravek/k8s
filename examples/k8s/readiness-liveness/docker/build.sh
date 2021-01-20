a25b0de6d7ebfaa52493f4713f956a2b9baf7a71
amoravek <amoravek@gmail.com>
2021-01-20 20:15:31 +0000

    updated

--- examples/k8s/readiness-liveness/docker/build.sh
+++ examples/k8s/readiness-liveness/docker/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 docker build -t k8s-sample-app:1.0 .
+docker tag k8s-sample-app:1.0 harbor.trask.cz/arm64/k8s-sample-app:1.0
+docker push harbor.trask.cz/arm64/k8s-sample-app:1.0
 
